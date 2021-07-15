b115b797148846cd52870983a1e03d8859e03529
Stephen Chang <stephen8chang@gmail.com>
2021-07-15 00:19:04 -0500

    removed fiber, added database connection

--- go/main.go
+++ go/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	db, err := gorm.Open(mysql.Open("root:Remake888!@/sys"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database!!")
+	}
+
+	fmt.Println(db)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
