5f72998d0a30731fac0fb342dc02265e0b0cb8a1
Stephen Chang <stephen8chang@gmail.com>
2021-07-26 22:08:29 -0500

    added middleware for permissions

---
+++ go/middlewares/permissionMiddleware.go
@@ -0,0 +1,51 @@
+package middlewares
+
+import (
+	"../database"
+	"../models"
+	"../util"
+	"github.com/gofiber/fiber"
+	"strconv"
+	"errors"
+)
+
+func IsAuthorized(c *fiber.Ctx, page string) error {
+	cookie := c.Cookies("jwt")
+
+	Id, err := util.ParseJwt(cookie)
+
+	if err != nil {
+		return err
+	}
+
+	userId, _ := strconv.Atoi(Id)
+
+	user := models.User{
+		Id: uint(userId),
+	}
+
+	database.DB.Preload("Role").Find(&user)
+
+	role := models.Role{
+		Id: user.RoleId,
+	}
+
+	database.DB.Preload("Permissions").Find(&role)
+
+	if c.Method() == "GET" {
+		for _, permission := ramge role.Permissions {
+			if permission.Name == "view_" + page || permission.Name == "edit_" + page {
+				return nil
+			}
+		}
+	} else {
+		for _, permission := ramge role.Permissions {
+			if permission.Name == "edit_" + page {
+				return nil
+			}
+		}
+	}
+
+	c.Status(fiber.StatusUnauthorized)
+	return errors.New("unauthorized")
+}
