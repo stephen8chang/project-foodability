7fe0e36075978ce7d7ea6a8f8ddd1dae9fa2b043
Stephen Chang <stephen8chang@gmail.com>
2021-07-26 21:55:48 -0500

    created order objects and associated classes

--- go/routes/routes.go
+++ go/routes/routes.go
@@ -41,4 +41,8 @@ func Setup(app *fiber.App) {
 	app.Post("/api/upload", controllers.Upload)
 	app.Static("/api/uploads", "./uploads")
 
+	app.Get("/api/orders", controllers.AllOrders)
+	app.Post("/api/export", controllers.Export)
+
+	app.Get("/api/chart", controllers.Chart)
 }
