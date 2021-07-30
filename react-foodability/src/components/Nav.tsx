6628fab9d9adf7d0a025d7dec3d0efaa793138b8
Stephen Chang <stephen8chang@gmail.com>
2021-07-30 00:02:02 -0500

    having issues displaying name on navbar

--- react-foodability/src/components/Nav.tsx
+++ react-foodability/src/components/Nav.tsx
@@ -1,19 +1,29 @@
-import React, {Component} from "react";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 
-class Nav extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-                <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/>
-                <ul className="navbar-nav px-3">
-                    <li className="nav-item text-nowrap">
-                        <a className="nav-link" href="#">Sign out</a>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+const Nav = () => {
+    const [user, setUser] = useState({
+        first_name: ''
+    });
+
+    useEffect(() => {
+        (
+            async () => {
+                const {data} = await axios.get('http://localhost:8000/api/user', {withCredentials: true});
+                setUser(data)
+            }
+        )();
+    }, []);
+    return (
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+            <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+
+            <ul className="navbar-nav px-3">
+                <a className="nav-link text-decoration-none" href="#">{user?.first_name}</a>
+                <a className="nav-link text-decoration-none" href="#">Sign out</a>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
