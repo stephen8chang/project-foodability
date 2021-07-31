de50c21b4e06b46463de270088806884b09ccef8
Stephen Chang <stephen8chang@gmail.com>
2021-07-31 01:04:07 -0500

    ability to update users

--- react-foodability/src/App.tsx
+++ react-foodability/src/App.tsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Route} from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import UserCreate from './pages/users/UserCreate';
+import UserEdit from './pages/users/UserEdit';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path={"/login"} component={Login}/>
         <Route path={"/users"} exact component={Users}/>
         <Route path={"/users/create"} component={UserCreate}/>
+        <Route path={"/users/:id/edit"} component={UserEdit}/>
       </BrowserRouter>
     </div>
   );
