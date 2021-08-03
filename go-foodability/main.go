6e5a1b060d54ca2852b4f2b986d26460fc2e834a
Stephen Chang <stephen8chang@gmail.com>
2021-08-03 00:35:46 -0500

    added changes to create container through docker

--- go-foodability/main.go
+++ go-foodability/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"./database"
 	"./routes"
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/middleware/cors"
 )
+
+func serveStatic(app *fiber.App) {
+	app.Static("/", "./build")
+}
 
 func main() {
 	database.Connect()
@@ -16,7 +23,16 @@ func main() {
 		AllowCredentials: true,
 	}))
 
+	serveStatic(app)
+
 	routes.Setup(app)
 
-	app.Listen(":8000")
+	// app.Listen(":8000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8000"
+
+	}
+	log.Printf("Listening on port %s\n\n", port)
+	app.Listen(port)
 }
